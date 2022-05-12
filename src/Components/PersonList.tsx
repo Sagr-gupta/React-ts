957aa032508a4c61904885ac52977249a2b480d1
Sagar@08092001 <Sagr-gupta>
2022-05-12 19:41:44 +0530

    Initial commit

---
+++ src/Components/PersonList.tsx
@@ -0,0 +1,19 @@
+import { Name } from "./Person.types";
+
+type personListProps = {
+  names: Name[];
+};
+
+export const PersonList = (props: personListProps) => {
+  return (
+    <div>
+      {props.names.map((name) => {
+        return (
+          <h2 key={name.first}>
+            {name.first} {name.last}
+          </h2>
+        );
+      })}
+    </div>
+  );
+};
